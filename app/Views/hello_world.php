613501a6ab83ec096e38928724dc21aaf89f0b35
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-30 21:41:13 +0700

    feat: first commit

---
+++ app/Views/hello_world.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World 🌎</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="h-screen flex flex-col items-center justify-center">
+        <h2 class="text-orange-600 font-bold text-[3rem]">
+            Hello World from CodeIgniter 4! the router is working!🔥 
+        </h2>
+        <p class="text-slate-600 font-semibold text-[1.5rem]">
+            Dimas Prihady Setyawan - 5025211184 - PBKK A
+        </p>
+    </div>
+</body>
+</html>
